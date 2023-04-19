381b7d05581f0dfa9a7d35b2b8c38ce70478045e
vladkyh <mraends@gmail.com>
2023-04-19 19:36:36 +0700

    poisk fiks admin

--- components/Admin/CreateCategoryModal.tsx
+++ components/Admin/CreateCategoryModal.tsx
@@ -4,36 +4,37 @@ import Button from "../common/Button";
 import Modal from "../common/Modal";
 import {createType} from "../../http/deviceAPI";
 
-const CreateCategoryModal = ({isOpen, onClose}) => (
-  <Modal
-    isOpen={isOpen}
-    onClose={onClose}
-    containerClassName={styles.categoryModal}
-  >
-    <form
-      className={styles.form}
-      onSubmit={(event) => {
-        event.preventDefault();
+const CreateCategoryModal = ({isOpen, onClose}) => {
 
-        const {name} = event.target as any;
+}
+  // <Modal
+  //   isOpen={isOpen}
+  //   onClose={onClose}
+  //   containerClassName={styles.categoryModal}
+  // >
+  //   <form
+  //     className={styles.form}
+  //     onSubmit={(event) => {
+  //       event.preventDefault();
 
-        if (!name.value)
-          return alert("Название категории не может быть пстой строкой");
+  //       const {name} = event.target as any;
 
-        createType({name: name.value})
-          .then((data) => {
-            alert(`Успешное создание категории ${data.name}`);
-          })
-          .catch((error) => {
-            alert(error.response.data.message);
-          });
-      }}
-    >
-      <Input name="name" placeholder="Категория" />
+  //       if (!name.value)
+  //         return alert("Название категории не может быть пстой строкой");
 
-      <Button type="submit">Создать категорию</Button>
-    </form>
-  </Modal>
-);
+  //       createType({name: name.value})
+  //         .then((data) => {
+  //           alert(`Успешное создание категории ${data.name}`);
+  //         })
+  //         .catch((error) => {
+  //           alert(error.response.data.message);
+  //         });
+  //     }}
+  //   >
+  //     <Input name="name" placeholder="Категория" />
+
+  //     <Button type="submit">Создать категорию</Button>
+  //   </form>
+  // </Modal>
 
 export default CreateCategoryModal;
